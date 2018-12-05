25c2c35e4f84a215c69af07186f2f5717e675b51
caoluyang8 <42856257+caoluyang8@users.noreply.github.com>
2018-12-05 15:26:16 +0800

    Add files via upload

---
+++ background-management-system/celery_conf.py
@@ -0,0 +1,4 @@
+BROKER_URL = "redis://127.0.0.1:6379/10"
+CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/11"
+TIME_ZONE = "Asia/Shanghai"
+# CELERY_ACCEPT_CONTENT = ['pickle', 'json', 'msgpack', 'yaml']
